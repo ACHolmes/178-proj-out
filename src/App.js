23d2c18a8f4660f9c2e625d31a92456f8c09f7bf
Sylvia <sylcrza@gmail.com>
2024-03-17 20:27:30 -0400

    add MapWrapper to app.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Navigation from './Navigation';
 import Component from './Component';
 
 function App() {
-
+  const [width, height] = [375, 812];
   return (
     <div>
       <Navigation />
+      <div style={{display: "flex", flexDirection:"row", width: "100%", justifyContent: "center"}}>
+        <MapWrapper width={width} height={height}/>
+      </div>
     </div>
   );
 }
