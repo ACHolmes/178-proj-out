dba74392854bf5cf393418bbe7f160e71bbd8793
Olafade Omole <olafadeomole@college.harvard.edu>
2024-03-18 01:57:35 -0400

    timeline base setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import Navigation from './Navigation';
 import MapWrapper from './MapWrapper';
+import Timeline from './Timeline';
 import { DeviceFrameset } from 'react-device-frameset'
 import 'react-device-frameset/styles/marvel-devices.min.css'
+
+const dumbStops = [ // very dumb, will probably have to restructure
+  { name: 'Stop 1', time: '8:00 AM' },
+  { name: 'Stop 2', time: '8:15 AM' },
+  { name: 'Stop 3', time: '8:30 AM' },
+  { name: 'Stop 4', time: '8:45 AM' },
+];
 
 function App() {
   const [width, height] = [375, 812];
   return (
     <div>
       <Navigation />
+      <Timeline stops={dumbStops}  />
       <div style={{display: "flex", flexDirection:"row", width: "100%", justifyContent: "center"}}>
         <DeviceFrameset device="iPhone X" width={width} height={height} zoom={0.9}>
-          <MapWrapper width={width} height={height}/>
+          <MapWrapper width={width} height={height} />
+          {/* <Timeline stops={dumbStops} /> */}
         </DeviceFrameset>
       </div>
     </div>
---
+++ src/Timeline.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './Timeline.css';
+
+const Timeline = ({ stops }) => {
+
+return (
+<div class="container">
+  <div class="wrapper">
+    <h2> Route Timeline </h2>
+    <ul class="sessions">
+    {stops.map((stop, index) => (
+      <li>
+        <div key={index} className="timeline-stop">
+          <div className="stop-name">{stop.name}</div>
+          <div className="stop-time">{stop.time}</div>
+        </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+</div> 
+);
+};
+
+export default Timeline;
+
